50914436e09f65efa7917032822a9fa00248eea3
auliaablqst <ab.auliaablqs@gmail.com>
2026-09-19 12:53:43 +0700

    refactor: hapus skrip JS legacy (buku.js & anggota.js)

--- simpus-mini/assets/js/anggota.js
+++
@@ -1,5 +0,0 @@
-function muatDaftarAnggota() {
-    return muatDataTabel("../data/anggota.json", ["no_anggota", "nama", "alamat", "no_hp"]);
-}
-
-document.addEventListener("DOMContentLoaded", muatDaftarAnggota);
