b5fadc2b2e568cf9aa9c0ae2a46920891179bbdb
auliaablqst <ab.auliaablqs@gmail.com>
2026-09-21 19:06:30 +0700

    feat: tambah kolom email dan tanggal_bergabung ke skema tabel anggota

--- simpus-mini/sql/01_buku_anggota.sql
+++ simpus-mini/sql/01_buku_anggota.sql
@@ -13,5 +13,7 @@ CREATE TABLE IF NOT EXISTS anggota (
     nama VARCHAR(255) NOT NULL,
     no_anggota VARCHAR(50) NOT NULL UNIQUE,
     alamat VARCHAR(255),
-    no_hp VARCHAR(30)
+    no_hp VARCHAR(30),
+    email VARCHAR(50),
+    tanggal_bergabung DATE DEFAULT CURRENT_DATE
 );
